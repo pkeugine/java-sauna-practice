64bf2551e48c4f15b91107f54aa52b6b0f686f87
besforyou <besforyou2@gmail.com>
2021-02-13 20:37:47 -0800

    feat(/src/main/java) add method to print locker data
    made a method to print out locker data
    occupation, fee, and locker number

--- src/main/java/Locker.java
+++ src/main/java/Locker.java
@@ -2,6 +2,10 @@ import java.util.Vector;
 
 public class Locker {
 
+    public static final String LOCKER_TOP = "┌ ────── ┐";
+    public static final String OCCUPATION_NUBMER = "| %d   %02d |";
+    public static final String LOCKER_BOTTOM = "└ ────── ┘";
+
     Locker() {
         occupation = new Vector<Integer>( );
         fee = new Vector<Integer>();
@@ -14,5 +18,28 @@ public class Locker {
 
     private Vector<Integer> occupation;
     private Vector<Integer> fee;
+
+    public void PrintLockers() {
+        PrintLockersFrom(0,9);
+        PrintLockersFrom(10,19);
+        PrintLockersFrom(20,29);
+        PrintLockersFrom(30,39);
+        PrintLockersFrom(40,49);
+    }
+
+    public void PrintLockersFrom(int start, int end) {
+        for(int i=start; i < end; i++) {
+            System.out.print(LOCKER_TOP);
+        }
+        System.out.println();
+        for(int i=start; i < end; i++) {
+            System.out.printf(OCCUPATION_NUBMER,occupation.get(i),fee.get(i));
+        }
+        System.out.println();
+        for(int i=start; i < end; i++) {
+            System.out.print(LOCKER_BOTTOM);
+        }
+        System.out.println();
+    }
 
 }
