2ff0b2bd2600bd9e7afb255362fcdeeede344db7
besforyou <besforyou2@gmail.com>
2021-02-13 22:44:38 -0800

    feat(src/main/java/) add method to AskUser class
    add method that prints out options the user can do
    after the user have choosen which locker to use

--- src/main/java/AskUser.java
+++ src/main/java/AskUser.java
@@ -4,5 +4,10 @@ public class AskUser {
         System.out.println("사우나 라커 번호를 입력해주시요. (q를 입력하여 프로그램 종료 가능)");
     }
 
-
+    public void AskWhatToDo(int LockerNumber) {
+        System.out.printf("%d번 라커에 어떤 것을 하시겠습니까?");
+        System.out.println("1. 손님 등록하기");
+        System.out.println("2. 요리 금액 추가하기");
+        System.out.println("3. 결제하고 퇴장하");
+    }
 }
