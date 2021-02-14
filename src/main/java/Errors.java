2f829e5576989119b4186a099043c5e31d26840a
besforyou <besforyou2@gmail.com>
2021-02-14 02:12:30 -0800

    feat(src/main/java/) add error printing class
    add class and methods that prints out errors
    when inappropriate input has been given from user

---
+++ src/main/java/Errors.java
@@ -0,0 +1,19 @@
+public class Errors {
+
+    public void WrongLockerNumber() {
+        System.out.println("[ ERROR ] 해당 라커 번호는 존재하지 않습니다.");
+    }
+
+    public void WrongMenuNumber() {
+        System.out.println("[ ERROR ] 유효하지 않은 메뉴입니다.");
+    }
+
+    public void WrongOperationSelected() {
+        System.out.println("[ ERROR ] 숫자를 입력해주세요.");
+    }
+
+    public void UnoccupiedLocker() {
+        System.out.println("[ ERROR ] 등록되어 있지 않은 라커입니다.");
+    }
+
+}
