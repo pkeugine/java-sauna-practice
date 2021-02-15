237ef638d76db6c52aaaee2119601b8bb0c8cc9f
besforyou <besforyou2@gmail.com>
2021-02-14 17:50:32 -0800

    feat(src/main/java/CheckNumber.java) methods to check the input number
    need to check if the input number from user is a number, and
    in range of [1,50]

--- src/main/java/CheckNumber.java
+++ src/main/java/CheckNumber.java
@@ -2,29 +2,57 @@ public class CheckNumber {
 
     Errors errors = new Errors();
 
-    Integer CheckGivenLockerNumber(String choice) {
-
-       Integer i = 0;
-
-       try {
-           i = Integer.parseInt(choice);
-       }
-       catch(IllegalArgumentException e){
-           errors.WrongLockerNumber();
-       }
-
-        return i;
-    }
-
-
-
-    public boolean isNumeric(String s) {
-        try{
-            Double.parseDouble(s);
-            return true;
-        } catch(NumberFormatException e) {
-            return false;
+    public Integer CheckGivenLockerNumber(String choice) {
+        if(CheckIfItISQ(choice)==true) {
+            return -1;
         }
+        if (CheckNumeric(choice)==0) {
+            errors.NeedToInputNumber();
+            return 0;
+        }
+        if ( CheckRange(choice) == 0 ) {
+            errors.WrongLockerNumber();
+            return 0;
+        }
+        return 1;
     }
 
+    public int CheckRange(String locker_num) {
+        int locker = Integer.parseInt(locker_num);
+        if (locker < 1 || locker > 50) {
+           return 0;
+        }
+        return 1;
+    }
+
+    public int CheckNumeric(String str) {
+        if (str == null) {
+            return 0;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return 0;
+        }
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            if (length == 1) {
+                return 0;
+            }
+            i = 1;
+        }
+        for (; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public boolean CheckIfItISQ(String input) {
+        if ( input.equalsIgnoreCase("Q")) {
+            return true;
+        }
+        return false;
+    }
 }
