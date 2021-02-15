95c89b8d0564fda3801e2c2bd4673a7fedabf86e
besforyou <besforyou2@gmail.com>
2021-02-15 05:00:32 -0800

    fix(src/main/java/) able to exit unoccupied lockers
    Able to exit lockers even if no one was using the locker
    Added line 4-7 at ExecuteOperationNumberThree.java class to fix problem

--- src/main/java/ExecuteOperationNumberThree.java
+++ src/main/java/ExecuteOperationNumberThree.java
@@ -1,6 +1,10 @@
 public class ExecuteOperationNumberThree {
 
     public static void ExecuteOperationNumberThreeMethod(int locker_number) {
+        if ( ExecuteOperation.CheckOccupationAt(locker_number) == false ) {
+            Errors.UnoccupiedLocker();
+            return ;
+        }
         Info.UserExitInfo(locker_number);
         Locker.ClearLockerNumer(locker_number);
         return ;
