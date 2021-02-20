794d9202a67900575a0ad0a650fb25858dda5c46
besforyou <besforyou2@gmail.com>
2021-02-19 22:13:04 -0800

    style(src/main/java/) reformatting %d and %s
    for better visual, changed %02d to %03d
    and changed %s to %-12s

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,6 @@
-public class player {
+public class Player {
 
-  Food f = new Food();
+  Food food = new Food();
 
   public void askWhichLockerToUse() {
     System.out.println();
@@ -10,7 +10,7 @@ public class player {
 
   public void askWhichOperationToExecute(int lockerNumber) {
     System.out.println();
-    System.out.printf("%d번 라커에 어떤 것을 하시겠습니까?",lockerNumber);
+    System.out.printf("%d번 라커에 어떤 것을 하시겠습니까?\n",lockerNumber);
     System.out.println("1. 손님 등록하기");
     System.out.println("2. 요리 금액 추가하기");
     System.out.println("3. 결제하고 퇴장하기");
@@ -20,25 +20,12 @@ public class player {
   public void printFoodMenu() {
     System.out.println();
     for (int i=1; i <= 10; i++) {
-      System.out.printf("[MENU]  [%02d] %10s :  %d 원\n",i,f.getFoodName(i),f.getFoodCost(i));
+      String foodName = food.getFoodName(i);
+      Integer foodCost = food.getFoodCost(i);
+      System.out.printf("[MENU]  [%03d] %-12s :  %5d 원\n",i,foodName,foodCost);
     }
     System.out.println();
     System.out.println("메뉴 번호를 골라주세요");
   }
-
-  public void printInfoOccupationSuccessful(int lockerNumber){
-    System.out.println();
-    System.out.printf("[INFO] %d번 라커에 손님이 등록되었습니다.\n",lockerNumber);
-    System.out.println();
-  }
-
-  public void printInfoFoodChoiceSuccessful(int lockerNumber, int foodNumber) {
-    Integer foodCost = f.getFoodCost(foodNumber);
-    String foodName = f.getFoodName(foodNumber);
-    System.out.println();
-    System.out.printf("[INFO] 러커 번호 %d번 %원을 추가하고 %s를 드렸습니다.\n",lockerNumber,foodCost,foodName);
-    System.out.println();
-  }
-
 
 }
