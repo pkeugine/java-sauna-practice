bb72237cac3333a0cb026f5d727c54387f05d9e9
besforyou <besforyou2@gmail.com>
2021-02-19 22:09:17 -0800

    feat(src/main/java/) user input validating methods
    need to check if user input is in range, numeric.

--- src/main/java/Validate.java
+++ src/main/java/Validate.java
@@ -1,5 +1,32 @@
 public class Validate {
 
+  public Integer firstValidityCheck(String input) {
+    if (checkIfQ(input)) {
+      return 0;
+    }
+    if (checkIfNumeric(input) && checkRange(input,1,50)) {
+      return 1;
+    }
+    return -1;
+  }
+
+  public Integer secondValidityCheck(String input) {
+    if (checkIfB(input)) {
+      return 0;
+    }
+    if (!checkIfNumeric(input) || !checkRange(input,1,3)) {
+      return -1;
+    }
+    return Integer.parseInt(input);
+  }
+  // return -1 if unvalid input, return proper menu number if valid input
+  public Integer foodMenuNumberValidtyCheck(String input) {
+    if (checkIfNumeric(input) && checkRange(input,1,10)) {
+      return Integer.parseInt(input);
+    }
+    return -1;
+  }
+
   public boolean checkIfNumeric(String input) {
     if (input.matches("-?\\d+")) {
       return true;
@@ -12,5 +39,20 @@ public class Validate {
       return true;
     }
     return false;
+  }
+
+  public boolean checkIfQ(String input) {
+    if (input.equalsIgnoreCase("Q")) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean checkRange(String input, int first, int last) {
+    Integer in = Integer.parseInt(input);
+    if (in < first || in > last) {
+      return false;
+    }
+    return true;
   }
 }
